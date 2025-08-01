5dbc1d5efbb03660262efa8c9578add72e51e178
pppatil7 <pundalikpatil7218@gmail.com>
2025-08-01 07:36:33 +0530

    update

--- InternalWorking/src/main/java/com/practice/InternalWorkingApplication.java
+++ InternalWorking/src/main/java/com/practice/InternalWorkingApplication.java
@@ -1,13 +1,21 @@
 package com.practice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InternalWorkingApplication {
+public class InternalWorkingApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InternalWorkingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InternalWorkingApplication.class, args);
+    }
 
+    private RazorPaymentService paymentService = new RazorPaymentService();
+
+    @Override
+    public void run(String... args) throws Exception {
+        String payment = paymentService.pay();
+        System.out.println("Payment Done.. " + payment);
+    }
 }
---
+++ InternalWorking/src/main/java/com/practice/RazorPaymentService.java
@@ -0,0 +1,12 @@
+package com.practice;
+
+public class RazorPaymentService {
+
+    public String pay() {
+        String payment = "Razorpay";
+        System.out.println("payment from: " + payment);
+        return payment;
+    }
+
+
+}
