9bf59f9e51c236ae2552f97cab578ab5ab5316f2
pppatil7 <pundalikpatil7218@gmail.com>
2025-08-01 08:19:10 +0530

    conditional bean config

--- InternalWorking/src/main/java/com/practice/RazorPaymentService.java
+++ InternalWorking/src/main/java/com/practice/RazorPaymentService.java
@@ -1,5 +1,10 @@
 package com.practice;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConditionalOnProperty(name = "payment.provider", havingValue = "razorpay")
 public class RazorPaymentService implements PaymentService {
 
     public String pay() {
