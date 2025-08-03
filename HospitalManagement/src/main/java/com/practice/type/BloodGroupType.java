611f5fdb91f63bff777398f6915bdacdd5c64848
pppatil7 <pundalikpatil7218@gmail.com>
2025-08-03 11:49:36 +0530

    data jpa

---
+++ HospitalManagement/src/main/java/com/practice/type/BloodGroupType.java
@@ -0,0 +1,14 @@
+package com.practice.type;
+
+public enum BloodGroupType {
+
+    A_POSITIVE,
+    A_NEGATIVE,
+    B_POSITIVE,
+    B_NEGATIVE,
+    AB_POSITIVE,
+    AB_NEGATIVE,
+    O_POSITIVE,
+    O_NEGATIVE
+
+}
