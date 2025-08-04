373fa97eb7db4880b06ad98b61e9a2dbb67b6f32
pppatil7 <pundalikpatil7218@gmail.com>
2025-08-04 09:52:03 +0530

    jpql

---
+++ HospitalManagement/src/main/java/com/practice/dto/BloodGroupCountResponseEntity.java
@@ -0,0 +1,20 @@
+package com.practice.dto;
+
+import com.practice.type.BloodGroupType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class BloodGroupCountResponseEntity {
+
+    private BloodGroupType bloodGroupType;
+
+    private Long count;
+
+
+}
