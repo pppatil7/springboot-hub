e73a483f72a8ff61af10e222b263e58b61e83e77
pp_ patil7 <pundalikpatil7218@gmail.com>
2025-08-17 14:40:37 +0530

    left join

--- HospitalManagement/src/main/java/com/practice/entity/Appointment.java
+++ HospitalManagement/src/main/java/com/practice/entity/Appointment.java
@@ -24,10 +24,12 @@ public class Appointment {
     private String reason;
 
     @ManyToOne
+    @ToString.Exclude
     @JoinColumn(name = "patient_id", nullable = false) //patient is required and not nullable
     private Patient patient;
 
     @ManyToOne
+    @ToString.Exclude
     @JoinColumn(nullable = false)
     private Doctor doctor;
 
