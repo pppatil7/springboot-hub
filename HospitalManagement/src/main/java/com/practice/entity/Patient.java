fcfb521b0bb4fcc28ccd394e10bcc60a0671c350
pp_ patil7 <pundalikpatil7218@gmail.com>
2025-08-17 13:15:23 +0530

    insurance service

--- HospitalManagement/src/main/java/com/practice/entity/Patient.java
+++ HospitalManagement/src/main/java/com/practice/entity/Patient.java
@@ -9,6 +9,7 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -49,11 +50,12 @@ public class Patient {
     @Enumerated(EnumType.STRING)
     private BloodGroupType bloodGroup;
 
-    @OneToOne
+    @OneToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
     @JoinColumn(name = "patient_insurance_id") //owning side
     private Insurance insurance;
 
     @OneToMany(mappedBy = "patient")
-    private List<Appointment> appointments;
+    @ToString.Exclude
+    private List<Appointment> appointments = new ArrayList<>();
 
 }
---
+++ HospitalManagement/src/main/java/com/practice/service/InsuranceService.java
@@ -0,0 +1,33 @@
+package com.practice.service;
+
+import com.practice.entity.Insurance;
+import com.practice.entity.Patient;
+import com.practice.repository.InsuranceRepository;
+import com.practice.repository.PatientRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class InsuranceService {
+
+
+    private final InsuranceRepository insuranceRepository;
+
+    private final PatientRepository patientRepository;
+
+
+    @Transactional
+    public Patient assignInsuranceToPatient(Insurance insurance, Long patientId) {
+        Patient patient = patientRepository.findById(patientId).
+                orElseThrow(() -> new EntityNotFoundException("Patient Not found with id: " + patientId));
+        patient.setInsurance(insurance);
+        insurance.setPatient(patient); //bidirectional consistency maintainance
+        return patient;
+
+    }
+
+
+}
