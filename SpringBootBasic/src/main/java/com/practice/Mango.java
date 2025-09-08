f3e2763e5f7a0504b33bdf403bd4dda2c2fab180
pp_ patil7 <pundalikpatil7218@gmail.com>
2025-09-08 17:28:46 +0530

    bean life cycle

--- SpringBootBasic/src/main/java/com/practice/Mango.java
+++ SpringBootBasic/src/main/java/com/practice/Mango.java
@@ -1,10 +1,22 @@
 package com.practice;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 public class Mango {
 
-    public void eat() {
+    void eat() {
         System.out.println("Eating mango");
+    }
+
+    @PostConstruct
+    void callThisBeforeMangoIsUsed() {
+        System.out.println("creating the mango before use");
+    }
+
+    @PreDestroy
+    void callThisBeforeDestroy() {
+        System.out.println("destroying the mango bean");
     }
 }
