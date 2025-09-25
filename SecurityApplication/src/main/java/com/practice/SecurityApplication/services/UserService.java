4d0d2c08072bfd80c053c4ad832e8fc4c710d8e1
pp_ patil7 <pundalikpatil7218@gmail.com>
2025-09-25 11:46:09 +0530

    userdetails

---
+++ SecurityApplication/src/main/java/com/practice/SecurityApplication/services/UserService.java
@@ -0,0 +1,22 @@
+package com.practice.SecurityApplication.services;
+
+import com.practice.SecurityApplication.exceptions.ResourceNotFoundException;
+import com.practice.SecurityApplication.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User with email " + username + " not found"));
+    }
+}
