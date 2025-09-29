375f41ced689ff4f1e2a4b08d0ce4ab77b00450b
pp_ patil7 <pundalikpatil7218@gmail.com>
2025-09-29 15:18:44 +0530

    logging

--- prod-ready/src/main/java/com/practice/clients/impl/EmployeeClientImpl.java
+++ prod-ready/src/main/java/com/practice/clients/impl/EmployeeClientImpl.java
@@ -4,6 +4,8 @@ import com.practice.clients.EmployeeClient;
 import com.practice.dto.EmployeeDto;
 import com.practice.exceptions.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -18,30 +20,47 @@ public class EmployeeClientImpl implements EmployeeClient {
 
     private final RestClient restClient;
 
+    Logger log = LoggerFactory.getLogger(EmployeeClientImpl.class);
+
     @Override
     public List<EmployeeDto> getAllEmployee() {
+        log.trace("Tying to retrieve all employees in getAllEmployees");
         try {
+            log.info("Attempting to call the restClient method in getAllEmployees");
             List<EmployeeDto> employeeDtoList = restClient.get()
                     .uri("employees")
                     .retrieve()
+                    .onStatus(HttpStatusCode::is4xxClientError, (req, res) -> {
+                        log.error(new String(res.getBody().readAllBytes()));
+                        throw new ResourceNotFoundException("could not create the employee");
+                    })
                     .body(new ParameterizedTypeReference<>() {
                     });
+            log.debug("Successfully retrieved the employees in getAllEmployees");
+            log.trace("Retrieved employees list in getAllEmployees {}", employeeDtoList.toString());
             return employeeDtoList;
         } catch (Exception e) {
+            log.error("Exception occurred in getAllEmployees", e);
             throw new RuntimeException(e);
         }
     }
 
     @Override
     public EmployeeDto getEmployeeById(Long employeeId) {
+        log.trace("Tying to retrieve all employees in getAllEmployees with id: {}", employeeId);
         try {
             EmployeeDto employeeDto = restClient.get()
                     .uri("employees/{employeeId}", employeeId)
                     .retrieve()
+                    .onStatus(HttpStatusCode::is4xxClientError, (req, res) -> {
+                        log.error(new String(res.getBody().readAllBytes()));
+                        throw new ResourceNotFoundException("could not create the employee");
+                    })
                     .body(new ParameterizedTypeReference<>() {
                     });
             return employeeDto;
         } catch (Exception e) {
+            log.error("Exception occurred in getEmployeeById", e);
             throw new RuntimeException(e);
         }
     }
